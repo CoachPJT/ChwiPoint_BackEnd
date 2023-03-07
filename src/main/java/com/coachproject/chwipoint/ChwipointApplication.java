645268b27dadbc4570620fd248e72c72ba61beff
ssooynn <2sy0127@gmail.com>
2023-03-07 10:37:36 +0900

    init: ChwiPoint_Backend

---
+++ src/main/java/com/coachproject/chwipoint/ChwipointApplication.java
@@ -0,0 +1,13 @@
+package com.coachproject.chwipoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChwipointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChwipointApplication.class, args);
+	}
+
+}
